54be969e9b1e35e46e38cfb9ac4bb78603c3042e
OSEMUDIAMEN ITUA <osemudiamenitua@gmail.com>
2021-08-30 22:12:12 +0100

    add endpoint to insert many

--- controllers/crud/write.js
+++ controllers/crud/write.js
@@ -14,7 +14,7 @@ exports.insertOne = async(req,res,next)=>{
         return next(new AppError("Collection name is required"))
     }
 
-    if(typeof(data)!=="object")
+    if(typeof(data)!=="object" || Array.isArray(data))
     {
         return next(new AppError("data should be an object"))
     }
@@ -22,4 +22,31 @@ exports.insertOne = async(req,res,next)=>{
     const doc = await db.collection(collection).insertOne(data)
 
     return res.status(200).json(doc)
+}
+
+
+
+exports.insertMany = async(req,res,next)=>{
+    const db = await connectToMongo()
+
+    const collection = req.params.collection
+    const data = req.body
+
+    if(!collection)
+    {
+        return next(new AppError("Collection name is required"))
+    }
+
+    //an array is expected. If its not an array then return an error.
+    if(!Array.isArray(data))
+    {
+        return next(new AppError("Array is expected. Array not found"))
+    }
+
+
+    const options = { ordered: true }
+
+    const docs = await db.collection(collection).insertMany(data,options)
+
+    return res.status(200).json(docs)
 }
